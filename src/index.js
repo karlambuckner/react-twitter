5345f0f8ada19ae851249744856c4cffd2c3e047
Karla Buckner <kmb2662@gmail.com>
2018-05-15 10:15:01 -0700

    compile with babel

--- src/index.js
+++
@@ -1,8 +0,0 @@
-var person = React.createElement('h3', {}, 'Karla Buckner');
-var user = React.createElement('p', {}, '@Karlambuckner');
-var app = React.createElement('div', {}, person, user);
-
-ReactDOM.render(
-  app,
-  document.getElementById('react-app-root')
-);
---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h4>Karla Buckner</h4>
+    <p>@Karlambuckner</p>
+  </div>,
+  document.getElementById('react-app-root')
+);
